d359bec68d173e1830dc0ee0baa57b8765e14b2e
Karl Horky <karl.horky@gmail.com>
2022-01-11 12:29:54 +0100

    Implement program

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
-const a = 1;
-console.log(a);
+// CommonJS version that didn't work:
+// var emoji = require('node-emoji');
+
+// ESM version that works:
+import emoji from 'node-emoji';
+
+const emojiInput = process.argv[2];
+
+// If the user has chosen an emoji
+if (emojiInput) {
+  // If there's no emoji found, print an error message
+  if (!emoji.hasEmoji(emojiInput)) {
+    console.log('Emoji not found');
+  } else {
+    // Print chosen emoji
+    console.log(emoji.get(emojiInput));
+  }
+} else {
+  // Print random emoji
+  console.log(emoji.random().emoji);
+}
--- readme.md
+++ readme.md
@@ -1,3 +1,26 @@
 # Node.js Random Emoji Generator
 
-Generate random emojis, optionally based on an input
+> Generate random emojis, optionally based on an input
+
+Without any user input, print random emoji:
+
+```bash
+$ node index.js
+🦄
+$ node index.js
+🍓
+```
+
+With user input of a correct emoji name, print emoji:
+
+```bash
+$ node index.js strawberry
+🍓
+```
+
+With user input of an incorrect emoji name, print error message that emoji doesn't exist:
+
+```bash
+$ node index.js strazzberry
+Emoji not found
+```
